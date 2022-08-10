9e690cd83978694c1d62407c9cb23eb8073d3e39
Aaron Elliott <mraarone@yahoo.com>
2022-08-09 23:58:16 -0500

    Added dockerfile ssh-agent support.

--- entrypoint.sh
+++ entrypoint.sh
@@ -1,7 +1,4 @@
 #!/bin/bash
 
 # This script is executed by the container at startup.
-
-# Install all of the requirements with pip, activate the virtualenv.
-pip install -r requirements.txt
-. .venv/bin/activate
+/bin/bash
