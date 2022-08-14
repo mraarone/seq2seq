c3eb4e9d92afd44a48cb2b543741a8ba5947be39
Aaron Elliott <mraarone@yahoo.com>
2022-08-14 17:53:06 -0500

    Corrected dockerfiles.

--- scripts/push.sh
+++ scripts/push.sh
@@ -3,5 +3,5 @@
 docker image tag pytorch-devenv:latest mraarone/pytorch-devenv:latest
 docker image push mraarone/pytorch-devenv:latest
 
-docker image tag pytorch-devenv:latest mraarone/pytorch-devenv:1.0.1
-docker image push mraarone/pytorch-devenv:1.0.1
+docker image tag pytorch-devenv:latest mraarone/pytorch-devenv:1.0.2
+docker image push mraarone/pytorch-devenv:1.0.2
